3ce57a585e8a76910f02dd9d20988a6ebda78d60
jonlima2018 <jonathan_martins_lima@yahoo.com.br>
2020-03-22 01:42:49 -0300

    incluido animação

---
+++ js/animate.js
@@ -0,0 +1,19 @@
+function initAnimacaoScroll(){
+
+    const sections = document.querySelectorAll('.js-scroll');
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+        sections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top;
+            const isSectionVisible = (sectionTop - windowMetade) < 0;
+            if(isSectionVisible){
+                section.classList.add('ativo');
+            }
+        });
+    }
+
+    window.addEventListener('scroll',animaScroll);
+}
+
+initAnimacaoScroll();
